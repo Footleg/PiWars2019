431bdc0b3c661ee1eaa08df68603d955554dd4d6
Footleg <drfootleg@gmail.com>
2019-02-27 16:49:34 +0000

    Refactored to enable virtual robot graphic representation to be used on real hardware

--- AdafruitPWMboard.py
+++ AdafruitPWMboard.py
@@ -9,8 +9,6 @@
     using this mocking module, so development and testing can be done without the
     actual robot being present.
 """
-import pygame
-import RobotControl as rc
 
 #Set servo min and max pulse length for the range of rotation of the servo model being used
 servoMin = 105  #105 Min pulse length (out of 4096)
@@ -19,31 +17,11 @@ servoRange = 180 #Rotation range in degrees of the servos being used
 motorPowerLimiting = 50 #Default limits motors to 50 power
 channelPulseLengths = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] #Store pulse lengths sent to each channel (for debug info)
 
-#Virtual hardware channels for mock robot
-servoChFL = 0
-servoChFR = 1
-servoChBL = 2
-servoChBR = 3
-motorChL1 = 12
-motorChL2 = 13
-motorChR1 = 14
-motorChR2 = 15
-
-#Globals to store positions of servos and motor channel percentage on
-motorChPctOnL1 = 0
-motorChPctOnR1 = 0
-motorChPctOnL2 = 0
-motorChPctOnR2 = 0
-servoPosFL = 0
-servoPosFR = 0
-servoPosBL = 0
-servoPosBR = 0
-
 
 def setServoPosition(channel, position):
     """ Sets the position of a servo in degrees
     """
-    global channelPulseLengths, servoPosFL, servoPosFR, servoPosBL, servoPosBR
+    global channelPulseLengths
     
     #Convert position in degrees to value in range min-max
     pulse = int( ( (servoMax - servoMin) * position / servoRange ) + servoMin)
@@ -51,25 +29,9 @@ def setServoPosition(channel, position):
     if (pulse < servoMin) or (pulse > servoMax):
         print("Calculated servo pulse {} is outside supported range of {} to {}".format(pulse,servoMin,servoMax) )
     else:
-        print("Setting servo {} pulse to {}".format(channel,pulse) )
-        validChannel = True
-        if channel == servoChFL:
-            servoPosFL = position
-        elif channel == servoChFR:
-            servoPosFR = position
-        elif channel == servoChBL:
-            servoPosBL = position
-        elif channel == servoChBR:
-            servoPosBR = position
-        else:
-            validChannel = False
-            displayError("Call to set servo position on channel {} which has no servo attached.".format(channel))
-        
-        print("ChL1:{},ChL2:{}".format(motorChPctOnL1,motorChPctOnL2) )
-
-        if validChannel:
-            drawVirtualRobot( pygame.display.get_surface() )
-            channelPulseLengths[channel] = pulse
+        #print("Setting servo {} pulse to {}".format(channel,pulse) )
+        #pwm.setPWM(channel, 0, pulse)
+        channelPulseLengths[channel] = pulse
 
     
 def setMotorPowerLimiting(percentage):
@@ -94,10 +56,10 @@ def setPercentageOn(channel, percent):
     """ Sets the percentage of time a channel is on per cycle.
         For use with PWM motor speed control.
     """
-    global channelPulseLengths, motorChPctOnL1, motorChPctOnR1, motorChPctOnL2, motorChPctOnR2
-
+    global channelPulseLengths
+    
     #Fully on pulse length is 4096.
-    #Limit to percentage of this using motorPowerLimiting value
+    #Scale this down using percentage power limiting global variable value
     maxPulse = 4096 * motorPowerLimiting / 100
     
     #Convert percentage to pulse length
@@ -109,26 +71,14 @@ def setPercentageOn(channel, percent):
     elif (percent > 100):
         pulse = maxPulse
 
-    #Now set actual percentage on calculated from pulse length which would be send to real hardware
+    #pwm.setPWM(channel, 0, pulse)
+    channelPulseLengths[channel] = pulse
+
+    ### Code for virtual robot representation below this line ###
+    
+    #Calculate actual percentage on from pulse length which would be sent to real hardware
     percentOn = int(pulse * 100 / 4096)
-    
     print("Motor channel {} requested power: {}, limited to {} by power limiting setting".format(channel, percent, percentOn) )
-    validChannel = True
-    if channel == motorChL1:
-        motorChPctOnL1 = percentOn
-    elif channel == motorChR1:
-        motorChPctOnR1 = percentOn
-    elif channel == motorChL2:
-        motorChPctOnL2 = percentOn
-    elif channel == motorChR2:
-        motorChPctOnR2 = percentOn
-    else:
-        validChannel = False
-        displayError("Call to set servo position on channel {} which has no servo attached.".format(channel))
-    
-    if validChannel:
-        drawVirtualRobot( pygame.display.get_surface() )
-        channelPulseLengths[channel] = pulse
         
     
 def allOff():
@@ -137,98 +87,6 @@ def allOff():
     
     channelPulseLengths = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     
-    
-def drawVirtualRobot(screen):
-    """ Draw graphical representation of robot on screen, indicating wheel positions and motor speeds """
-    
-    #Reset screen with a background image
-    image = pygame.image.load( "mars_hubble_canyon.jpg" ).convert()
-    screen.blit( image, [0, 0] )
-    
-    #Draw body of robot
-    BLACK = [0,0,0]
-    BROWN = [120,60,0]
-    RED   = [255,0,0]
-    BLUE  = [0,0,200]
-    PURPLE = [100,0,100]
-    WHEEL_W = 30
-    WHEEL_H = 70
-    pygame.draw.rect(screen, BLUE, pygame.Rect(335,145,130,175))
-    pygame.draw.rect(screen, BLUE, pygame.Rect(365,30,70,115))
-    pygame.draw.rect(screen, BLUE, pygame.Rect(290,160,220,16))
-    pygame.draw.rect(screen, BLUE, pygame.Rect(296,100,20,280))
-    pygame.draw.rect(screen, BLUE, pygame.Rect(484,100,20,280))
-    pygame.draw.rect(screen, BLUE, pygame.Rect(270,220,30,18))
-    pygame.draw.rect(screen, BLUE, pygame.Rect(500,220,30,18))
-    pygame.draw.rect(screen, BLACK, pygame.Rect(240,195,WHEEL_W,WHEEL_H))
-    pygame.draw.rect(screen, BLACK, pygame.Rect(530,195,WHEEL_W,WHEEL_H))
-        
-    #Draw rotated wheels
-    CIRCLE_DIA = 8
-    WHEEL_L_X = 306
-    WHEEL_R_X = 494
-    WHEEL_F_Y = 62
-    WHEEL_B_Y = 418
-    #Adjust for 90 degrees being zero position of virtual wheels, and apply servo calibration offsets
-    drawRotatedRectangle(screen,WHEEL_L_X,WHEEL_B_Y,WHEEL_W,WHEEL_H,90-servoPosBL+rc.servoRearLeftOffset,BLACK)
-    pygame.draw.circle(screen, RED,(WHEEL_L_X,WHEEL_B_Y),CIRCLE_DIA)
-    drawRotatedRectangle(screen,WHEEL_R_X,WHEEL_B_Y,WHEEL_W,WHEEL_H,90-servoPosBR+rc.servoRearRightOffset,BLACK)
-    pygame.draw.circle(screen, RED,(WHEEL_R_X,WHEEL_B_Y),CIRCLE_DIA)
-    drawRotatedRectangle(screen,WHEEL_L_X,WHEEL_F_Y,WHEEL_W,WHEEL_H,90-servoPosFL+rc.servoFrontLeftOffset,BLACK)
-    pygame.draw.circle(screen, RED,(WHEEL_L_X,WHEEL_F_Y),CIRCLE_DIA)
-    drawRotatedRectangle(screen,WHEEL_R_X,WHEEL_F_Y,WHEEL_W,WHEEL_H,90-servoPosFR+rc.servoFrontRightOffset,BLACK)
-    pygame.draw.circle(screen, RED,(WHEEL_R_X,WHEEL_F_Y),CIRCLE_DIA)
-    
-    #Display motor speeds
-    font = pygame.font.Font(None, 28)
-    motorSpeedL = motorChPctOnL2 - motorChPctOnL1
-    textBitmap = font.render("{}%".format(motorSpeedL), True, RED )
-    arrowLength = 20 + abs(motorSpeedL * 2)
-    arrowTop = 230-(arrowLength/2)
-    pygame.draw.rect(screen, PURPLE, pygame.Rect(160,arrowTop,30,arrowLength))
-    if motorSpeedL > 0:
-        pygame.draw.polygon(screen, PURPLE, [(175,arrowTop-30),(145,arrowTop),(205,arrowTop)])
-    elif motorSpeedL < 0:
-        arrowBot = arrowTop + arrowLength
-        pygame.draw.polygon(screen, PURPLE, [(175,arrowBot+30),(145,arrowBot),(205,arrowBot)])
-    screen.blit( textBitmap, (160,220) )
-
-    motorSpeedR = motorChPctOnR2 - motorChPctOnR1
-    textBitmap = font.render("{}%".format(motorSpeedR), True, RED )
-    arrowLength = 20 + abs(motorSpeedR * 2)
-    arrowTop = 230-(arrowLength/2)
-    pygame.draw.rect(screen, PURPLE, pygame.Rect(620,230-(arrowLength/2),30,arrowLength))
-    if motorSpeedR < 0:
-        pygame.draw.polygon(screen, PURPLE, [(635,arrowTop-30),(605,arrowTop),(665,arrowTop)])
-    elif motorSpeedR > 0:
-        arrowBot = arrowTop + arrowLength
-        pygame.draw.polygon(screen, PURPLE, [(635,arrowBot+30),(605,arrowBot),(665,arrowBot)])
-    screen.blit( textBitmap, (620,220) )
-    
-
-def drawRotatedRectangle(screen,x,y,w,h,rot,colour):
-    GREEN = [0,255,0]
-    # define a surface (rectangle) which can be transformed  
-    image1 = pygame.Surface((w, h))  
-    # set colour to be transparent background for rotating an image  
-    image1.set_colorkey(GREEN)  
-    # fill the rectangle / surface with solid color  
-    image1.fill(colour)  
-    # transform the orignal image (-rot to +ve rotation is clockwise)
-    image2 = pygame.transform.rotate(image1 , -rot)  
-    rect = image2.get_rect()  
-    # set the centre position of rotated rectangle to the desired position  
-    rect.center = (x, y)  
-    # drawing the rotated rectangle to the screen  
-    screen.blit(image2 , rect)
-    
-    
-def displayError(message):
-    screen = pygame.display.get_surface()
-    screen.fill( (0,0,0) )
-    font = pygame.font.Font(None, 32)
-    textBitmap = font.render(message, True, [255,0,0] )
-    screen.blit( textBitmap, (20, 200) )
     
 def main():
     """ Test function for module
--- RobotControl.py
+++ RobotControl.py
@@ -5,6 +5,7 @@
 
 import AdafruitPWMboard as pwmb
 import time
+import pygame
 
 # PWM board channel configuration 
 servoFrontLeftChannel = 0
@@ -24,6 +25,12 @@ servoRearRightOffset = 24 #24 <- plywood prototype
 
 servoMinAngle = 40
 servoMaxAngle = 135
+
+#Globals to store positions servos have been set to
+servoPosFL = 0
+servoPosFR = 0
+servoPosBL = 0
+servoPosBR = 0
 
 
 #==========================================================================================
@@ -46,22 +53,39 @@ def setSteeringLegPositionSafely(angle, servoChannel, servoOffset):
     
     #Set servo position
     pwmb.setServoPosition(servoChannel, servoPos) 
+    drawVirtualRobot( pygame.display.get_surface() )
+    
     
 def setSteeringFrontLeft(angle):
     """ Sets the position of the front left steering servo """
+    global servoPosFL
+    
     setSteeringLegPositionSafely(angle, servoFrontLeftChannel, servoFrontLeftOffset)
+    servoPosFL = angle
+    
     
 def setSteeringFrontRight(angle):
     """ Sets the position of the front right steering servo """
+    global servoPosFR
+    
     setSteeringLegPositionSafely(angle, servoFrontRightChannel, servoFrontRightOffset)
+    servoPosFR = angle
+    
     
 def setSteeringRearLeft(angle):
     """ Sets the position of the rear left steering servo """
+    global servoPosBL
+    
     setSteeringLegPositionSafely(angle, servoRearLeftChannel, servoRearLeftOffset)
+    servoPosBL = angle
+    
     
 def setSteeringRearRight(angle):
     """ Sets the position of the rear right steering servo """
+    global servoPosBR
+    
     setSteeringLegPositionSafely(angle, servoRearRightChannel, servoRearRightOffset)
+    servoPosBR = angle
     
     
 def spotTurnSteering(angle):
@@ -117,18 +141,117 @@ def setMotorPower(channelA, channelB, power):
     else:
         pwmb.setPercentageOn(channelA, 0)
         pwmb.setPercentageOn(channelB, 0)
+    
+    drawVirtualRobot( pygame.display.get_surface() )
+
 
 def setLeftMotorPower(power):
     setMotorPower(motorsLeftChannelA, motorsLeftChannelB, power)
     
+    
 def setRightMotorPower(power):
     setMotorPower(motorsRightChannelA, motorsRightChannelB, power)
+    
     
 def stopAll():
     pwmb.allOff()
     
+    
 def drawVirtualRobot(screen):
-    pwmb.drawVirtualRobot(screen)
+    """ Draw graphical representation of robot on screen, indicating wheel positions and motor speeds """
+    
+    if screen != None:
+        #Reset screen with a background image
+        image = pygame.image.load( "mars_hubble_canyon.jpg" ).convert()
+        screen.blit( image, [0, 0] )
+        
+        #Draw body of robot
+        BLACK = [0,0,0]
+        BROWN = [120,60,0]
+        RED   = [255,0,0]
+        BLUE  = [0,0,200]
+        PURPLE = [100,0,100]
+        WHEEL_W = 30
+        WHEEL_H = 70
+        pygame.draw.rect(screen, BLUE, pygame.Rect(335,145,130,175))
+        pygame.draw.rect(screen, BLUE, pygame.Rect(365,30,70,115))
+        pygame.draw.rect(screen, BLUE, pygame.Rect(290,160,220,16))
+        pygame.draw.rect(screen, BLUE, pygame.Rect(296,100,20,280))
+        pygame.draw.rect(screen, BLUE, pygame.Rect(484,100,20,280))
+        pygame.draw.rect(screen, BLUE, pygame.Rect(270,220,30,18))
+        pygame.draw.rect(screen, BLUE, pygame.Rect(500,220,30,18))
+        pygame.draw.rect(screen, BLACK, pygame.Rect(240,195,WHEEL_W,WHEEL_H))
+        pygame.draw.rect(screen, BLACK, pygame.Rect(530,195,WHEEL_W,WHEEL_H))
+            
+        #Draw rotated wheels
+        CIRCLE_DIA = 8
+        WHEEL_L_X = 306
+        WHEEL_R_X = 494
+        WHEEL_F_Y = 62
+        WHEEL_B_Y = 418
+        #Adjust for 90 degrees being zero position of virtual wheels, and apply servo calibration offsets
+        drawRotatedRectangle(screen,WHEEL_L_X,WHEEL_B_Y,WHEEL_W,WHEEL_H,90-servoPosBL,BLACK)
+        pygame.draw.circle(screen, RED,(WHEEL_L_X,WHEEL_B_Y),CIRCLE_DIA)
+        drawRotatedRectangle(screen,WHEEL_R_X,WHEEL_B_Y,WHEEL_W,WHEEL_H,90-servoPosBR,BLACK)
+        pygame.draw.circle(screen, RED,(WHEEL_R_X,WHEEL_B_Y),CIRCLE_DIA)
+        drawRotatedRectangle(screen,WHEEL_L_X,WHEEL_F_Y,WHEEL_W,WHEEL_H,90-servoPosFL,BLACK)
+        pygame.draw.circle(screen, RED,(WHEEL_L_X,WHEEL_F_Y),CIRCLE_DIA)
+        drawRotatedRectangle(screen,WHEEL_R_X,WHEEL_F_Y,WHEEL_W,WHEEL_H,90-servoPosFR,BLACK)
+        pygame.draw.circle(screen, RED,(WHEEL_R_X,WHEEL_F_Y),CIRCLE_DIA)
+        
+        #Display motor speeds
+        font = pygame.font.Font(None, 28)
+        motorSpeedL = int( (pwmb.channelPulseLengths[motorsLeftChannelB]-pwmb.channelPulseLengths[motorsLeftChannelA]) * 100 / 4096)
+        textBitmap = font.render("{}%".format(motorSpeedL), True, RED )
+        arrowLength = 20 + abs(motorSpeedL * 2)
+        arrowTop = 230-(arrowLength/2)
+        pygame.draw.rect(screen, PURPLE, pygame.Rect(160,arrowTop,30,arrowLength))
+        if motorSpeedL > 0:
+            pygame.draw.polygon(screen, PURPLE, [(175,arrowTop-30),(145,arrowTop),(205,arrowTop)])
+        elif motorSpeedL < 0:
+            arrowBot = arrowTop + arrowLength
+            pygame.draw.polygon(screen, PURPLE, [(175,arrowBot+30),(145,arrowBot),(205,arrowBot)])
+        screen.blit( textBitmap, (160,220) )
+
+        motorSpeedR = int( (pwmb.channelPulseLengths[motorsRightChannelB]-pwmb.channelPulseLengths[motorsRightChannelA]) * 100 / 4096)
+        textBitmap = font.render("{}%".format(motorSpeedR), True, RED )
+        arrowLength = 20 + abs(motorSpeedR * 2)
+        arrowTop = 230-(arrowLength/2)
+        pygame.draw.rect(screen, PURPLE, pygame.Rect(620,230-(arrowLength/2),30,arrowLength))
+        if motorSpeedR < 0:
+            pygame.draw.polygon(screen, PURPLE, [(635,arrowTop-30),(605,arrowTop),(665,arrowTop)])
+        elif motorSpeedR > 0:
+            arrowBot = arrowTop + arrowLength
+            pygame.draw.polygon(screen, PURPLE, [(635,arrowBot+30),(605,arrowBot),(665,arrowBot)])
+        screen.blit( textBitmap, (620,220) )
+    else:
+        print("Unable to draw robot when screen is not initialised in pygame.")
+        
+
+def drawRotatedRectangle(screen,x,y,w,h,rot,colour):
+    GREEN = [0,255,0]
+    # define a surface (rectangle) which can be transformed  
+    image1 = pygame.Surface((w, h))  
+    # set colour to be transparent background for rotating an image  
+    image1.set_colorkey(GREEN)  
+    # fill the rectangle / surface with solid color  
+    image1.fill(colour)  
+    # transform the orignal image (-rot to +ve rotation is clockwise)
+    image2 = pygame.transform.rotate(image1 , -rot)  
+    rect = image2.get_rect()  
+    # set the centre position of rotated rectangle to the desired position  
+    rect.center = (x, y)  
+    # drawing the rotated rectangle to the screen  
+    screen.blit(image2 , rect)
+    
+    
+def displayError(message):
+    screen = pygame.display.get_surface()
+    screen.fill( (0,0,0) )
+    font = pygame.font.Font(None, 32)
+    textBitmap = font.render(message, True, [255,0,0] )
+    screen.blit( textBitmap, (20, 200) )
+    
     
 def main():
     """ Test function for servos and motors
