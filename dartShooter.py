c9e373ccec10bc3af545751f9a8cc0f77ff34ada
footleg <drfootleg@gmail.com>
2019-03-24 12:36:00 +0000

    Cleaned up comments and recalibrated firing trigger servo

--- dartShooter.py
+++ dartShooter.py
@@ -10,36 +10,40 @@ escChannel = 15
 
 maxMotorPower = int( 180 * 0.6 ) #Above 60% max power the  rubber band comes off motor
 
+triggerOpenPos = 85
+
+triggerClosedPos = 160
+
 
 def motorOn():
-    """ Turns on the nerf shooter motor by ramping up power """
+    """ Turns on the dart shooter motor by ramping up power """
     for angle in range(0, maxMotorPower):
         pwmb.setServoPosition(escChannel, angle) 
         time.sleep(0.1)
 
 
 def motorOff():
-    """ Turns off the nerf shooter motor """
+    """ Turns off the dart shooter motor """
     pwmb.setPercentageOn(escChannel, 0) #Off
     
     
 def laserOn():
-    """ Turns off the nerf shooter motor """
+    """ Turns off the laser """
     pwmb.setConstantOn(laserChannel)
     
 
 def laserOff():
-    """ Turns off the nerf shooter motor """
+    """ Turns off the laser """
     pwmb.setPercentageOn(laserChannel, 0) #Off
 
 
 def fire():
     """ Pushes dart onto flywheel """
-    for angle in range(90, 180, 15):
+    for angle in range(triggerOpenPos, triggerClosedPos, 5):
         pwmb.setServoPosition(triggerChannel, angle) 
         time.sleep(0.05)
     time.sleep(0.5)
-    pwmb.setServoPosition(triggerChannel, 90)
+    pwmb.setServoPosition(triggerChannel, triggerOpenPos)
     
     
 def main():
@@ -47,6 +51,7 @@ def main():
     # Arm ESC and start up motor
     # ===========================================================================
     try:
+        
         print("Setting min pulse to arm (for 2 seconds)")
         pwmb.setServoPosition(escChannel, 0)
         time.sleep(2)
@@ -63,7 +68,7 @@ def main():
         time.sleep(5)
         fire()
         time.sleep(5)
-        fire()
+        laserOn()
         time.sleep(1)
 
     finally:
